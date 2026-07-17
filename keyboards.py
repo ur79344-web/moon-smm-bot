ae4807cea62acaff0f547726b317d642602dc0e0
ur79344-web <ur79344@gmail.com>
2026-07-17 17:25:13 +0500

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,1 +1,12 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+main_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="🛒 Xizmatlar")],
+        [KeyboardButton(text="📦 Buyurtma"), KeyboardButton(text="💰 Balans")],
+        [KeyboardButton(text="📋 Buyurtmalarim")],
+        [KeyboardButton(text="➕ Hisobni to'ldirish")],
+        [KeyboardButton(text="📞 Admin")]
+    ],
+    resize_keyboard=True
+)
