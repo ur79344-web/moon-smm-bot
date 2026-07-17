be60968869b2a75d9c1347d2fdd4a648e5177a6d
ur79344-web <ur79344@gmail.com>
2026-07-17 16:56:57 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,22 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+TOKEN = "8858080639:AAGUlxbFZ-2AjbFnY8jNW6bKw_dXJZoNri4"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer(
+        "🌙 MOON SMM\n\n"
+        "Bot ishga tushdi! ✅"
+    )
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
