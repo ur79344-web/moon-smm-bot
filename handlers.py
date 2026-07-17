78090e58a4f517e1c2166d4b4b67773b464c9f01
ur79344-web <ur79344@gmail.com>
2026-07-17 17:29:59 +0500

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,1 +1,24 @@
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from keyboards import main_menu
+from database import add_user, create_db
 
+router = Router()
+
+
+@router.message(CommandStart())
+async def start(message: Message):
+    await create_db()
+
+    await add_user(
+        message.from_user.id,
+        message.from_user.username
+    )
+
+    await message.answer(
+        "🌙 MOON SMM\n\n"
+        "Xush kelibsiz!\n"
+        "Kerakli bo'limni tanlang:",
+        reply_markup=main_menu
+    )
