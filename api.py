880eed4146bc7803fbea8fb481bbbf3df5665451
ur79344-web <ur79344@gmail.com>
2026-07-17 17:23:25 +0500

    Update api.py

--- api.py
+++ api.py
@@ -1,1 +1,47 @@
+import aiohttp
+from config import API_URL, API_KEY
 
+async def get_services():
+    if not API_URL or not API_KEY:
+        return []
+
+    data = {
+        "key": API_KEY,
+        "action": "services"
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(API_URL, data=data) as response:
+            return await response.json()
+
+
+async def create_order(service, link, quantity):
+    if not API_URL or not API_KEY:
+        return {"error": "API sozlanmagan"}
+
+    data = {
+        "key": API_KEY,
+        "action": "add",
+        "service": service,
+        "link": link,
+        "quantity": quantity
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(API_URL, data=data) as response:
+            return await response.json()
+
+
+async def order_status(order_id):
+    if not API_URL or not API_KEY:
+        return {"error": "API sozlanmagan"}
+
+    data = {
+        "key": API_KEY,
+        "action": "status",
+        "order": order_id
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(API_URL, data=data) as response:
+            return await response.json()
